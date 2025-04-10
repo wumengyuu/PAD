ed7311cd5a5d70fc9d4266300e2c0ab6bae72384
Mengyu Wu <mengyu.wu@stud.uni-goettingen.de>
2025-04-10 15:00:18 +0100

    add files

--- function.py
+++ function.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 def normalization_range(data):
     mean = data.mean()
@@ -12,3 +14,23 @@ def normalization_zscore(data):
     z_scores = (data - mean) / std
     return z_scores
 
+def caulate_eingenvalues_eingenvectors(x_norm):
+    """
+    Calculate the eigenvalues and eigenvectors of the covariance matrix of the normalized data.
+    Args:
+        x_norm (numpy.ndarray): Normalized data.
+    Returns:
+        e (numpy.ndarray): Eigenvalues of the covariance matrix.
+        v (numpy.ndarray): Eigenvectors of the covariance matrix.
+    """
+    # Covariance matrix
+    covmatrix = np.cov(x_norm.T)
+
+    # Obtain the eingenvalues and eingenvectors of covariance matrix
+    e, v = np.linalg.eig(covmatrix)
+
+    # order descendingly by largest eigenvalue
+    order = np.argsort((np.argsort(e) * -1))
+    e = e[order]
+    v = v[:,order]
+    return e, v
